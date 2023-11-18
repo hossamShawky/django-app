8a43a1ae5206e72adf86cf9a49244f5213f204dc
hossam_shawky <hossamshawky288@gmail.com>
2023-11-19 00:48:35 +0200

    Run Migration and Create Models

--- dashboard/app/models.py
+++ dashboard/app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    name = models.CharField(max_length=15)
+
+class Post(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    desc = models.CharField(max_length=200)
+ 
